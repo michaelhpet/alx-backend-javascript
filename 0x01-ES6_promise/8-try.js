c45c35ca042975bc14a5489e0be6f96e88cdb9a7
Michael Peter <michaelhpet@gmail.com>
2024-02-17 00:12:41 +0100

    fix: correct spellings

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,4 @@
 export default function divideFunction(numerator, demoninator) {
-  if (demoninator === 0) throw new Error('Cannot divide by 0');
+  if (demoninator === 0) throw new Error('cannot divide by 0');
   return numerator / demoninator;
 }
