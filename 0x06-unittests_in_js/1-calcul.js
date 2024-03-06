d28ac1e18e1408f03f219f74e070e0c12d61391f
Michael Peter <michaelhpet@gmail.com>
2024-03-06 23:07:57 +0100

    feat: upgrade calculateNumber function

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,10 @@
+function calculateNumber(type, a, b) {
+  if (typeof type !== 'string') throw new Error('type must be a string');
+  if (!['SUM', 'SUBTRACT', 'DIVIDE'].includes(type))
+    throw new Error('Invalid type provided');
+  const _a = Math.round(a);
+  const _b = Math.round(b);
+  if (type === 'SUM') return _a + _b;
+  else if (type === 'SUBTRACT') return _a - _b;
+  return _a / _b;
+}
